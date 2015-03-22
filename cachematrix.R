9f6d78e49529ecd37e9b95dfe04ef300398a0c04
Albertedm <dapena@gmail.com>
2015-03-22 19:42:25 +0100

    Create the body of makeCacheMatrix and cacheSolve. Create test.R to test if the solution works

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions are used to cached the inverse of a given matrix, in order to avoid 
+## unnecesary computations of that value
 
-## Write a short comment describing this function
-
+## makeCacheMatrix creates a special "matrix", which is really a list containing a function to
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the inverse
+## get the value of the inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        # If the matrix changes, we must reset the cached content
+        inverse <<- NULL
+    }
+    get <- function()  {
+        x
+    }
+    setinverse <- function(inv) {
+        inverse <<- inv
+    }
+    getinverse <- function() {
+        inverse
+    }
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve returns the inverse of the special "matrix" created with the above function.
+## However, it first checks to see if the inverse has already been calculated.
+## If so, it gets the inverse from the cache and skips the computation.
+## Otherwise, it calculates the inverse of the matrix and sets the value of the inverse in the cache via the setinverse function.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinverse(inv)
+    inv
 }
---
+++ test.R
@@ -0,0 +1,7 @@
+x <- matrix(1:4, nrow = 2, ncol = 2)
+
+cachedMatrix <- makeCacheMatrix(x)
+
+sol <- cacheSolve(cachedMatrix)
+
+
